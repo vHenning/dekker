ba9cc8febb6e4639c54593feef52b59dd9b73711
Henning Vehring <henning.vehring@gmail.com>
2023-09-01 23:05:01 -0600

    Remove ChatGPT's sample data

--- main.py
+++ main.py
@@ -4,17 +4,7 @@ import matplotlib.pyplot as plt
 # Create a directed graph
 g = nx.DiGraph()
 
-# Add nodes
-g.add_node("A")
-g.add_node("B")
-g.add_node("C")
-g.add_node("D")
 
-# Add edges with directions
-g.add_edge("A", "B")
-g.add_edge("B", "C")
-g.add_edge("C", "D")
-g.add_edge("D", "A")
 
 # Draw the graph
 pos = nx.spring_layout(g)  # Positions of nodes for visualization
