3b27281a4005b56059362cea77934b8a399edeaf
Henning Vehring <henning.vehring@gmail.com>
2023-09-01 21:39:46 -0600

    Add logic for process p

--- ProcessState.py
+++ ProcessState.py
@@ -1,3 +1,5 @@
+import copy
+
 class ProcessState:
     def __init__(self):
         self.pWants = False
@@ -7,6 +9,44 @@ class ProcessState:
         # Yes, we do start counting at 1 to be consistent with lecture notes
         self.pProgramCounter = 1
         self.qProgramCounter = 1
+
+    # Returns the next process state if p's program counter advances.
+    # Returns the same process state if p cannot advance
+    def pStep(self):
+        newState = copy.deepcopy(self)
+        if (self.pProgramCounter == 1):
+            newState.pProgramCounter = 2
+        if (self.pProgramCounter == 2):
+            newState.pProgramCounter = 3
+            newState.pWants = True
+        if (self.pProgramCounter == 3):
+            if (self.qWants == True):
+                newState.pProgramCounter = 4
+            else:
+                newState.pProgramCounter = 8
+        if (self.pProgramCounter == 4):
+            if (self.turn == 2):
+                newState.pProgramCounter = 5
+            else:
+                newState.pProgramCounter = 3
+        if (self.pProgramCounter == 5):
+            newState.pProgramCounter = 6
+            newState.pWants = False
+        if (self.pProgramCounter == 6):
+            if (self.turn == 1):
+                newState.pProgramCounter = 7
+        if (self.pProgramCounter == 7):
+            newState.pProgramCounter = 3
+            newState.pWants = True
+        if (self.pProgramCounter == 8):
+            newState.pProgramCounter = 9
+        if (self.pProgramCounter == 9):
+            newState.pProgramCounter = 10
+            newState.turn = 2
+        if (self.pProgramCounter == 10):
+            newState.pProgramCounter = 1
+            newState.pWants = False
+        return newState
 
     def boolToString(self, bool):
         if (bool == True):
